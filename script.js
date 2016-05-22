e7d0fd48f2701ecb7f74564ec7ba458a49a11048
pyboy5152 <parkerstorm013@gmail.com>
2016-05-22 00:55:30 +0000

    hOIgit status gIT goT cOmmITgit status

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
 
+  $(".homePage").mouseenter(function() {
+
+    $(this).animate({
+      height: "+=15px"
+    });
+
+    $(this).fadeTo('fast', 1);
+  });
+
+  $(".homePage").mouseLeave(function() {
+    $(this).animate({
+      height: "-=15px"
+    });
+    $(this).fadeTo("fast", 0.5);
+  });
+  $('div').mouseenter(function() {
+    $(this).fadeTo('fast', 1);
+  });
+  $('div').mouseLeave(function() {
+    $(this).fadeTo('fast', 0.5);
+  });
+  });
 });
