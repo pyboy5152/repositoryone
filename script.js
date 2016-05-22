f0df9a6a5bcab0216624aed7a540cc93513e5d0b
pyboy5152 <parkerstorm013@gmail.com>
2016-05-22 03:27:01 +0000

    Committed

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
 $(document).ready(function() {
 
-  $(".homePage").mouseenter(function() {
-
-    $(this).animate({
-      height: "+=15px"
-    });
-
-    $(this).fadeTo('fast', 1);
-  });
-
-  $(".homePage").mouseLeave(function() {
-
-    $(this).animate({
-      height: "-=15px"
-    });
-
-    $(this).fadeTo("fast", 0.5);
-  });
-
-  $('div').mouseenter(function() {
-
-    $(this).fadeTo('fast', 1);
-  });
-
-  $('div').mouseLeave(function() {
-
-    $(this).fadeTo('fast', 0.5);
-
-  });
-
-});
-
 });
